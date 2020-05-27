759224216082537f68f0735ad7ee0646ec119392
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-28 00:07:10 +0200

    Learn how to use axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  // can modify request here
+  return request;
+}, error  => {
+  console.log(error);
+  Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
