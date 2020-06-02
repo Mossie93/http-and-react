584b63a8c7649735038a12cc6ac86dae5761e864
Rafal Mossakowski <rmossakowski@gmail.com>
2020-06-02 20:04:44 +0200

    Add route to /new-post page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -17,7 +18,8 @@ class Blog extends Component {
           </nav>
         </header>
         <Route path="/" exact component={Posts}/>
-        <Route path="/posts" exact component={Posts}/>
+        <Route path="/posts" component={Posts}/>
+        <Route path="/new-post" component={NewPost}/>
       </div>
     );
   }
