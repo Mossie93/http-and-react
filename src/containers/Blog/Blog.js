7cff9785f47eac1df5040d58501a921b00c8e610
Rafal Mossakowski <rmossakowski@gmail.com>
2020-06-02 02:24:15 +0200

    Extract some logic from Blog container to the newly created Posts container

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,40 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-import Post from '../../components/Post/Post';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null
-  }
-
-  componentDidMount() {
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return { ...post, author: 'Max' }
-        });
-        this.setState({posts: updatedPosts});
-      });
-  }
-
-  postSelectedHanlder = (id) => {
-    this.setState({selectedPostId: id});
-  }
-
   render () {
-    const posts = this.state.posts.map(post => {
-      return <Post 
-        key={post.id} 
-        title={post.title} 
-        author={post.author}
-        clicked={() => {this.postSelectedHanlder(post.id)}}
-      />
-    });
-
     return (
       <div className="Blog">
         <header>
@@ -45,9 +15,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">
-          {posts}
-        </section>
+        <Posts/>
       </div>
     );
   }
