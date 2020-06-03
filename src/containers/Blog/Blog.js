b3233f5a20ca365d2344f994458054e95caffb9d
Rafal Mossakowski <rmossakowski@gmail.com>
2020-06-04 00:39:42 +0200

    Learn how to use nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch } from 'react-router-dom';
 
-import FullPost from './FullPost/FullPost';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
@@ -13,17 +12,14 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><NavLink exact to="/">Home</NavLink></li>
               <li><NavLink to="/posts">Posts</NavLink></li>
               <li><NavLink to="/new-post">New Post</NavLink></li>
             </ul>
           </nav>
         </header>
         <Switch>
-          <Route path="/" exact component={Posts}/>
           <Route path="/posts" component={Posts}/>
           <Route path="/new-post" component={NewPost}/>
-          <Route path="/:id" component={FullPost}/>
         </Switch>
       </div>
     );
