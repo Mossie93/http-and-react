a167a3cc5148eca1fea80abab2248ac6e62b0ec5
Rafal Mossakowski <rmossakowski@gmail.com>
2020-06-04 20:53:30 +0200

    Lazyloading components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
 
   render () {
@@ -22,7 +26,7 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
           <Route path="/posts" component={Posts}/>
           <Route render={()=><h1>404, not found!</h1>}/>
         </Switch>
